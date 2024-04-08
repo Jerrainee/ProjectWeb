ba566245a4c5d63029669cea210adbadef05ddd1
Jerrainee <ilinspavel07@gmail.com>
2024-04-08 17:48:21 +0300

    login form added

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import datetime
 
 from flask import Flask, render_template, redirect
-from flask_login import LoginManager
+from flask_login import LoginManager, login_user
 
 from data import db_session
 from forms.user import RegisterForm, LoginForm
@@ -53,6 +53,21 @@ def reqister():
         db_sess.commit()
         return redirect('/login')
     return render_template('register.html', title='Регистрация', form=form)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        db_sess = db_session.create_session()
+        user = db_sess.query(User).filter(User.email == form.email.data).first()
+        if user and user.check_password(form.password.data):
+            login_user(user, remember=form.remember_me.data)
+            return redirect("/")
+        return render_template('login.html',
+                               message="Неправильный логин или пароль",
+                               form=form)
+    return render_template('login.html', title='Авторизация', form=form)
 
 
 @app.route('/test_run/<int:n>', methods=['GET', 'POST'])
